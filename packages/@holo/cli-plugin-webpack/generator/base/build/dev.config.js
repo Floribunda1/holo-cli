97a10fc90f6a2fd3f0c0207c74fc22ad33143a9c
Floribunda1 <1094083688@qq.com>
2021-09-30 11:42:27 +0800

    chore: 同步 webpack 最新配置

--- packages/@holo/cli-plugin-webpack/generator/base/build/dev.config.js
+++ packages/@holo/cli-plugin-webpack/generator/base/build/dev.config.js
@@ -4,18 +4,19 @@ const { merge } = require('webpack-merge');
 const common = require('./base.config');
 
 const devConfig = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    contentBase: path.resolve(__dirname, 'dist'),
-    hot: true,
-		quiet: true,
-  },
-  devtool: 'inline-source-map',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+	mode: 'development',
+	devServer: {
+		port: 8080,
+		static: {
+			directory: path.resolve(__dirname, 'dist'),
+		},
+		hot: true,
+	},
+	devtool: 'inline-source-map',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
 };
 
 module.exports = merge(common, devConfig);
